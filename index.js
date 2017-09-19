08f1b631c1d3dfe2f382f3db4d84c3b887033a4d
minecrawler <me@marco-alka.de>
2017-09-19 15:36:27 +0200

    fix #1

--- index.js
+++ index.js
@@ -3,4 +3,5 @@
 module.exports = {
     Option: require('./interface/rustify-option.h'),
     Result: require('./interface/rustify-result.h'),
+    registerAllGlobals: require('./src/rustify.registerAllGlobals.c'),
 };
